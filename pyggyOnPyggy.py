49320486e6d09de347bfc9cd07238602b1767c03
Kory Mathewson <korymath@gmail.com>
2016-04-04 09:59:48 -0600

    Live test with new database, sorted folder organization
    Using MongoDB for speed, this is the commit for the Youtube video.

--- pyggyOnPyggy.py
+++ pyggyOnPyggy.py
@@ -14,20 +14,30 @@ import os.path
 bot = ChatBot("One",
     storage_adapter="chatterbot.adapters.storage.MongoDatabaseAdapter",
     logic_adapters=[
-        "chatterbot.adapters.logic.EvaluateMathematically",
-        "chatterbot.adapters.logic.TimeLogicAdapter",
+        # "chatterbot.adapters.logic.EvaluateMathematically",
+        # "chatterbot.adapters.logic.TimeLogicAdapter",
         # "chatterbot.adapters.logic.ClosestMeaningAdapter"
+
+        # closest match seems to work better and faster than the closest meaning
         "chatterbot.adapters.logic.ClosestMatchAdapter"
     ],
     io_adapters=[
-        "chatterbot.adapters.io.JsonAdapter"
+        "chatterbot.adapters.io.NoOutputAdapter"
     ],
-    database="database_movie_small_mongo")
+    database="database_movie_2000_good")
+
+## TODO: you have to run mongod for the databse to get rolling.
+## 4000 is too slow (1-3 seconds per response)
+# But the answers are great, and it doesn't get lost.
+## 1000 is about half of a second, could up this to 2000 random conversations. 
+## this timing is scaling linearly ,as would be expected.
 
 # check if the database file exists
 # if not, then train it
-bot.train("chatterbot.corpus.english.movielines10000")
-bot.train("chatterbot.corpus.english.conversations")
+bot.train("chatterbot.corpus.english.movielines2000rand")
+
+# 16 seconds for 4000 conversation corpuss
+# bot.train("chatterbot.corpus.english.conversations")
 	
 host = 'aiaas.pandorabots.com'
 user_key = uk
@@ -55,9 +65,11 @@ botname = 'pyggy2'
 def speak_who(text, name):
 	print(name + " said: " + text)
 	if name == 'Fiona':
-		system("say -v Tom -r 200 " + text) # should be Tom
+		return True
+		# system("say -v Tom -r 200 " + text) # should be Tom
 	elif name == 'Pyggy':
-		system("say -v Samantha -r 200 you said: " + text) # should be Samantha
+		# system("say -v Samantha -r 200 you said: " + text) # should be Samantha
+		return True
 	else:
 		return
 
@@ -91,32 +103,51 @@ with open('logs/' + str(timenow) + '_log.csv', 'wb') as csvfile:
 			# Fiona says something (chatterbot)
 			speak_response(text1, 'Fiona')
 
-			# human responds 
+			## Human spoken responses
 			# audio = r.listen(source)
 			# try:
 				# text2 = r.recognize_google(audio)
 			# except:
 				# text2 = 'Lets start again'
-
-			text2 = str(raw_input())
-
-			speak_response(text2, 'You')
+			# text2 = str(raw_input())
+			# speak_response(text2, 'You')
 
 			# Pyggy responds (pandorabot)
-			# result = API.talk(user_key, app_id, host, botname, text1, session_id=True, recent=True)
-			# pb_text2 = result['response']
-			# botresponse2 = bot.get_response(botresponse1)
-			# text2 = pb_text2 + ' ' + botresponse2
-			# speak_response(text2, 'Pyggy')
+			try:
+				result = API.talk(user_key, app_id, host, botname, text1, session_id=True, recent=True)
+			except:
+				result = "But I could be all wrong."
+
+			pb_text2 = result['response']
+			botresponse2 = bot.get_response(botresponse1)
+			text2 = pb_text2 + ' ' + botresponse2
+			speak_response(text2, 'Pyggy')
 
 			# Save the back and forth to the CSV log
-			# writer.writerow([time.time(),text1,text2])
-			# csvfile.flush()
+			newtime = time.time()
+			time_elapsed = newtime-timenow
+			# print 'first time: ' + str(time_elapsed)
 
-			result = API.talk(user_key, app_id, host, botname, text2, session_id=True, recent=True)
+			writer.writerow([newtime,newtime-timenow,text1,text2])
+			csvfile.flush()
+
+			try:
+				result = API.talk(user_key, app_id, host, botname, text2, session_id=True, recent=True)
+			except:
+				result = "But I could be all wrong."
 			pb_text1 = result['response']
-			# for talking with pyggy
-			# botresponse1 = bot.get_response(botresponse2)
-			botresponse1 = bot.get_response(text2)
+
+			newtime = time.time()
+			time_elapsed = newtime-timenow
+			# print 'pandora time: ' + str(time_elapsed)
+			
+			## When talking with pyggy only respond to the stem 
+			botresponse1 = bot.get_response(botresponse2)
+			# botresponse1 = bot.get_response(text2)
+
+			newtime = time.time()
+			time_elapsed = newtime-timenow
+			# print 'chatter time: ' + str(time_elapsed)
+			
 			text1 = pb_text1 + ' ' + botresponse1
 
